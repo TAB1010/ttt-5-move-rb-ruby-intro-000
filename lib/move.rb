f8431dfc285cf54d17e1372b14c7358543b9b3da
KHANDKER HASSAN <khassanasgar@gmail.com>
2020-05-28 21:04:12 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,26 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def move(board, index, type="X")
+  arr = board
+  arr[index.to_i-1] = type
+  arr
+end
+ruby
+def number_adder(n)
+  n += 10
+end
+
+def array_adder(a)
+  a << "new thing at the end of the array"
+end
+
+x = 10
+puts "Before call #{x}"
+number_adder(x)
+puts "After call: #{x}: Holy moly, unchanged!"
+
+z = [1, 'hi', "Byron"]
+puts "Before call #{z}"
+array_adder(z)
+puts "After call #{z}: Holy moly, *changed*!"
